b8161b378c904b992947a8f54368dc63aeded163
Ankit Jangir <ankit3jangir574@gmail.com>
2026-02-02 16:46:11 +0530

    ass

--- index.js
+++ index.js
@@ -5,20 +5,12 @@ import morgan from "morgan";
 import { connectDB } from "../db/db.js";              
 import registerRouter from "../Router/registerRouter.js"; 
 
-dotenv.config();
+const PORT = process.env.PORT || 5000;
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(morgan("dev"));
-
-connectDB();
-
-app.get("/api", (req, res) => {
-  res.send("Backend running 🚀");
-});
-
-app.use("/api/user", registerRouter);
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
 export default app;
