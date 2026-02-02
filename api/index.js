8a81c0196374d76fa5e0dae386a0f9a8511205e8
Ankit Jangir <ankit3jangir574@gmail.com>
2026-02-02 14:53:03 +0530

    fix vercel 404

--- api/index.js
+++ api/index.js
@@ -1,25 +1,28 @@
 import express from "express";
 import cors from "cors";
-import morgan from "morgan";
 import dotenv from "dotenv";
-import { connectDB } from "../db/db.js";
-import registerRouter from "../Router/registerRouter.js";
+import morgan from "morgan";
+import { connectDB } from "./db/db.js";
+import registerRouter from "./Router/registerRouter.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use(morgan("dev"));
 app.use(express.json());
+app.use(morgan("dev"));
 
 connectDB();
 
 app.get("/", (req, res) => {
-  res.send("Backend is running on Vercel 🚀");
+  res.send("Backend running 🚀");
 });
 
 app.use("/api/user", registerRouter);
 
+const PORT = process.env.PORT || 8080;
 
-export default app;
+app.listen(PORT, () => {
+  console.log("Server running on", PORT);
+});
