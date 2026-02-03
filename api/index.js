00a850cd226c194a84ddb14cab649b9d65d5fb0a
Ankit Jangir <ankit3jangir574@gmail.com>
2026-02-03 15:24:53 +0530

    all done

--- api/index.js
+++ api/index.js
@@ -7,13 +7,13 @@ import { connectDB } from "../db/db.js";
 import registerRouter from "../Router/registerRouter.js";
 
 dotenv.config();
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+// Database connect
 connectDB();
 
 app.get("/api", (req, res) => {
@@ -21,5 +21,13 @@ app.get("/api", (req, res) => {
 });
 
 app.use("/api/user", registerRouter);
+
+// Local testing ke liye (Vercel par ye ignore ho jayega)
+const PORT = process.env.PORT || 8080;
+if (process.env.NODE_ENV !== "production") {
+    app.listen(PORT, () => {
+        console.log(`Server started on http://localhost:${PORT}/api`);
+    });
+}
 
 export default serverless(app);
