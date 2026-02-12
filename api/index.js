fcac13a38721d88b8331645f8f446c3864f0f458
Ankit Jangir <ankit3jangir574@gmail.com>
2026-02-12 12:41:42 +0530

    fix: minimal backend test

--- api/index.js
+++ api/index.js
@@ -1,25 +1,14 @@
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import connectDB from "../db/db.js";
-import registerRouter from "../Router/registerRouter.js";
 
-dotenv.config();
 const app = express();
- 
- const port = 8080;
 
-app.use(cors());
 app.use(express.json());
 
-
-await connectDB();
-    
-
 app.get("/", (req, res) => {
-    res.send({ message: "Backend is live on Vercel 🚀" });
+  res.status(200).json({
+    success: true,
+    message: "Backend running on Vercel ✅"
+  });
 });
-
-app.use("/api", registerRouter);
 
 export default app;
