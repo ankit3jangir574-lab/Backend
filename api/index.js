ac557f465f820e4ad3c47b2830ef94e61b018bdf
Ankit Jangir <ankit3jangir574@gmail.com>
2026-02-12 13:10:56 +0530

    remove builds warning

--- api/index.js
+++ api/index.js
@@ -1,11 +1,25 @@
 import express from "express";
-import connectDB from "./db.js";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "../db/db.js";
+import registerRouter from "../Router/registerRouter.js";
 
+dotenv.config();
 const app = express();
+ 
+ const port = 8080;
 
-app.get("/", async (req, res) => {
-  await connectDB();
-  res.send("Backend + MongoDB working 🚀");
+app.use(cors());
+app.use(express.json());
+
+
+await connectDB();
+    
+
+app.get("/", (req, res) => {
+    res.json({ message: "Backend is live on Vercel 🚀" });
 });
+
+app.use("/api", registerRouter);
 
 export default app;
